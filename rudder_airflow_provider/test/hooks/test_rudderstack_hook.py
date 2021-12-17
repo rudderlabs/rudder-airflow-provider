1cea76b9a59f92c9dd0f36bdb7e7f0c6fd3d9308
Yeshwanth Vuppu <yeshwanth@rudderstack.com>
2021-12-17 15:37:11 +0530

    Add tests for hook

---
+++ rudder_airflow_provider/test/hooks/test_rudderstack_hook.py
@@ -0,0 +1,79 @@
+import json
+import unittest
+from unittest import mock
+from airflow.exceptions import AirflowException
+
+from airflow.models.connection import Connection
+from requests.models import Response
+from rudder_airflow_provider.hooks.rudderstack import RudderstackHook
+
+
+class RudderstackHookTest(unittest.TestCase):
+
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.get_connection')
+    def test_get_access_token(self, mocked_http: mock.Mock):
+        rudder_connection = Connection(password='some-password')
+        mocked_http.return_value = rudder_connection
+        hook = RudderstackHook('rudderstack_connection', '12345')
+        self.assertEqual(hook.get_access_token(), 'some-password')
+
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.get_connection')
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.run')
+    def test_trigger_sync(self, mock_run: mock.Mock, mock_connection: mock.Mock):
+        source_id = 'some-source-id'
+        access_token = 'some-password'
+        hook = RudderstackHook('rudderstack_connection', source_id)
+        mock_connection.return_value = Connection(password=access_token)
+        sync_endpoint = f"/v2/sources/{source_id}/start"
+        mock_run.return_value = Response()
+        hook.trigger_sync()
+        mock_run.assert_called_once_with(endpoint=sync_endpoint, headers={
+                 'authorization': f"Bearer {access_token}"})
+
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.get_connection')
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.run')
+    def test_triger_sync_exception(self, mock_run: mock.Mock, mock_connection: mock.Mock):
+        source_id = 'some-source-id'
+        access_token = 'some-password'
+        mock_connection.return_value = Connection(password=access_token)
+        mock_run.side_effect = AirflowException()
+        hook = RudderstackHook('rudderstack_connection', source_id)
+        self.assertRaises(AirflowException, hook.trigger_sync)
+
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.get_connection')
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.run')
+    def test_poll_status(self, mock_run: mock.Mock, mock_connection: mock.Mock):
+        source_id = 'some-source-id'
+        access_token = 'some-password'
+        status_endpoint = f"/v2/sources/{source_id}/status"
+        finished_status_response = Response()
+        finished_status_response.status_code = 200
+        finished_status_response.json = mock.MagicMock(return_value={'status': 'finished'})
+        mock_run.return_value = finished_status_response
+        mock_connection.return_value = Connection(password=access_token)
+        hook = RudderstackHook('rudderstack_connection', source_id)
+        hook.poll_for_status()
+        mock_run.assert_called_once_with(endpoint=status_endpoint, headers={
+                 'authorization': f"Bearer {access_token}"})
+
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.get_connection')
+    @mock.patch('rudder_airflow_provider.hooks.rudderstack.HttpHook.run')
+    def test_poll_status_failure(self, mock_run: mock.Mock, mock_connection: mock.Mock):
+        source_id = 'some-source-id'
+        access_token = 'some-password'
+        status_endpoint = f"/v2/sources/{source_id}/status"
+        finished_status_response = Response()
+        finished_status_response.status_code = 200
+        finished_status_response.json = mock.MagicMock(
+            return_value={'status': 'finished', 'error': 'some-eror'})
+        mock_run.return_value = finished_status_response
+        mock_connection.return_value = Connection(password=access_token)
+        hook = RudderstackHook('rudderstack_connection', source_id)
+        self.assertRaises(AirflowException, hook.poll_for_status)
+        mock_run.assert_called_once_with(endpoint=status_endpoint, headers={
+                 'authorization': f"Bearer {access_token}"})
+
+
+
+if __name__ == '__main__':
+    unittest.main()
